2c54244b196e6fcb9f4ae663b5677a57fd2864a4
Juan Oliver <juanoliver995@gmail.com>
2022-11-08 11:49:35 -0300

    cambie el index

--- src/Paginas/Index.jsx
+++ src/Paginas/Index.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import HeroImage from '../components/HeroImage'
 import HeroVideo from '../components/HeroVideo'
 import Header from '../components/Header'
@@ -8,39 +7,23 @@ import Music from '../components/Music'
 import Galery from '../components/Galery'
 import Footer from '../components/Footer'
 import SocialLinks from '../components/SocialLinks'
-import Loader from '../components/Loader'
+
 
 const Index = () => {
-  const [loading, setLoading] = useState(true)
-  const [dates, setDates] = useState([])
 
-
-  useEffect(() => {
-    fetch('https://andres-back-production.up.railway.app/api/dates')
-      .then((res) => res.json())
-      .then((data) => setDates(data))
-      .catch((err) => console.log(err))
-    setLoading(false)
-  },[])
 
   return (
     <div>
+      <Header />
       {
-        loading ? <Loader loading={loading} />
-          :
-          <>
-            <Header />
-            {
-              window.screen.width > 480 ? <HeroImage /> : <HeroVideo />
-            }
-            <Biography />
-            <Dates dates={dates} />
-            <Music />
-            <Galery />
-            <SocialLinks />
-            <Footer />
-          </>
+        window.screen.width > 480 ? <HeroImage /> : <HeroVideo />
       }
+      <Biography />
+      <Dates />
+      <Music />
+      <Galery />
+      <SocialLinks />
+      <Footer />
     </div>
   )
 }
