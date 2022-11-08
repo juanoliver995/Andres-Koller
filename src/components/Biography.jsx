effd2d9deabb993cad5ea890cb2fa0577c96ec7d
Juan Oliver <juanoliver995@gmail.com>
2022-11-08 14:04:36 -0300

    cambios en la bio

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -26,7 +26,7 @@ const Biography = () => {
                   parties from Argentina, Mexico, the United States
                   and Chile. Sharing the booth with various national
                   and international artists such as, Adam Beyer,
-                  Victor Ruiz, Pan Pot, Nicolas Taboada, Beico and
+                  Victor Ruiz, Pan Pot, Deborah De Luca, The Elements, Sasha Carassi Nicolas Taboada, Beico and
                   others. Today he dedicates all his time to the music
                   to show the public his way of creating a unique
                   style when it comes to creating a DJ set and also
@@ -45,7 +45,7 @@ const Biography = () => {
           viewport={{ once: true }}
           className='download-bio'
         >
-          <a href='../../public/AndresKoller-Biography.pdf' download="Andres-Koller-Biography">Download Biography</a>
+          <a href='../public/AndresKoller-Biography.pdf' download="Andres-Koller-Biography">Download Biography</a>
         </motion.div>
       </div>
       <div className='img-biography'>
