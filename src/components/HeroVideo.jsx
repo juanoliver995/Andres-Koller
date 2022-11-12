8e65fae8905266e7e9c68ef11b96b44a7e1d5313
Juan Oliver <juanoliver995@gmail.com>
2022-11-12 11:43:43 -0300

    quite sonido de video

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -1,52 +1,10 @@
 import video from '../assets/andres-web.mp4'
-import soundOn from '../assets/sound-on.svg'
-import soundOff from '../assets/sound-off.svg'
-import { useState } from 'react'
-import MutedVideo from './MutedVideo'
 
 const HeroVideo = () => {
-  const [muted, setMuted] = useState(true)
-  const [viewMuted, setViewMuted] = useState(true)
 
-  const userMutedAccion = () => {
-    setMuted(true)
-    setViewMuted(false)
-    const video = document.getElementById('video-hero')
-    video.play()
-    video.muted = true
-
-  }
-  const userUnMutedAccion = () => {
-    setMuted(false)
-    setViewMuted(false)
-    const video = document.getElementById('video-hero')
-    video.play()
-    video.muted = false
-  }
-
-  const mutedVideo = () => {
-    const video = document.getElementById('video-hero')
-    if (muted === true) {
-      setMuted(false)
-      video.muted = false
-    } else {
-      setMuted(true)
-      video.muted = true
-    }
-  }
   return (
     <div className='hero-video'>
-      {
-        viewMuted ? <MutedVideo userMutedAccion={userMutedAccion} userUnMutedAccion={userUnMutedAccion} /> : ''
-      }
-      <video src={video} playsInline loop preload="auto" id="video-hero"/>
-      <div className='container-sound'>
-        <button onClick={() => mutedVideo()} className="btn-muted">
-          {
-            !muted ? <img src={soundOn} /> : <img src={soundOff} />
-          }
-        </button>
-      </div>
+      <video src={video} muted={true} autoPlay={true} loop id="video-hero"/>
     </div>
   )
 }
