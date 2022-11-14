e4ac2b2f2565969a7d6eb984519332ec867d77b4
Juan Oliver <juanoliver995@gmail.com>
2022-11-14 16:34:23 -0300

    pruebas del video en ios

--- src/components/HeroVideo.jsx
+++ src/components/HeroVideo.jsx
@@ -4,11 +4,9 @@ import video from '../assets/andres-web.mp4'
 
 const HeroVideo = () => {
 
-
-
   return (
     <div className='hero-video'>
-      <video muted={true} autoPlay={true} src={video} loop preload="auto" id="video-hero"/>
+      <video muted={true} autoPlay={true} src={video} playsInline loop preload="auto" id="video-hero"/>
     </div>
   )
 }
