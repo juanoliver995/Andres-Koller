3c2ce5263b54aead4eb72cc7cd10e033abd63c6d
Juan Oliver <juanoliver995@gmail.com>
2023-07-25 19:10:06 -0300

    mejora vista telefono

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,7 +9,7 @@ const Modal = ({ setModal } ) => {
       <button onClick={handleModal}>
         <img src="../../icons8-close.svg"/>
       </button>
-      <iframe src="https://www.youtube.com/embed/-swyyPb9HrA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+      <iframe src="https://www.youtube.com/embed/-swyyPb9HrA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
     </div>
   )
 }
