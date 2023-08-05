53975f848e9f1aeb42d2f6c7d886ed1cf1c0e1f2
Juan Oliver <juanoliver995@gmail.com>
2023-08-04 21:53:14 -0300

    subi set de layton giordani

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -6,6 +6,18 @@ const Music = () => {
       <div className="title-music">
         <h2>Listen To My Sets</h2>
       </div>
+      <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1573565839&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+      <div style={{
+        fontSize: '10px',
+        color: '#000000',
+        linebreak: 'anywhere',
+        wordBreak: 'normal',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textOverflow: 'ellipsis',
+        fontFamily: 'Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif',
+        fontWeight: '100'
+      }}></div>
       <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1394276782&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
       <div style={{
         fontSize: '10px',
